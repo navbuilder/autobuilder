5eeb0abc4c126529f8bc5b15d2b2b100cc6e7930
navbuilder <57806386+navbuilder@users.noreply.github.com>
2019-11-15 18:11:57 +0100

    Variable declaration

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 set -e 
 
-export REPO=$1
-export BRANCH=$2
-export PR=$3
-export URL=https://github.com/$REPO/navcoin-core.git
-exort  ACCESS_TOKEN=g17h084cc351704e1
+REPO=$1
+BRANCH=$2
+PR=$3
+URL=https://github.com/$REPO/navcoin-core.git
+ACCESS_TOKEN=g17h084cc351704e1
+JOBS=7
+MEMORY=24000
 
 rm -rf ~/navcoin-core ; cd ~ && git clone ${URL} --branch ${BRANCH}
 cd ~/navcoin-core && git pull ; git checkout ${BRANCH} ; git pull
@@ -24,21 +26,21 @@ fi
 
 make -C ~/navcoin-core/depends download SOURCES_PATH=`pwd`/cache/common
 cd ~/gitian-builder &&\
-USE_DOCKER=1 ./bin/gbuild --memory 24000 -j7 --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-win.yml &&\
+USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY} -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-win.yml &&\
 mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz ~/public_html/binaries/$BRANCH/ &&\
 cp var/install.log ~/public_html/binaries/$BRANCH/log/win/ &&\
 cp var/build.log ~/public_html/binaries/$BRANCH/log/win/ &&\
 cd ~/public_html/binaries/$BRANCH && rm *SHA256SUM* ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
 
 cd ~/gitian-builder &&\
-USE_DOCKER=1 ./bin/gbuild --memory 24000  -j7 --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-osx.yml &&\
+USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY}  -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-osx.yml &&\
 mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz ~/public_html/binaries/$BRANCH/ &&\
 cp var/install.log ~/public_html/binaries/$BRANCH/log/osx/ &&\
 cp var/build.log ~/public_html/binaries/$BRANCH/log/osx/ &&\
 cd ~/public_html/binaries/$BRANCH && rm *SHA256SUM* ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
 
 cd ~/gitian-builder &&\
-USE_DOCKER=1 ./bin/gbuild --memory 24000  -j7 --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-linux.yml &&\
+USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY}  -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-linux.yml &&\
 mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz ~/public_html/binaries/$BRANCH/ &&\
 cp var/install.log ~/public_html/binaries/$BRANCH/log/linux/ &&\
 cp var/build.log ~/public_html/binaries/$BRANCH/log/linux/ &&\
@@ -51,7 +53,7 @@ if (( $PR > 0 ));
 then
    curl -s -H "Authorization: token ${ACCESS_TOKEN}" \
     -X POST -d '{"body": "A new build of ${LASTCOMMIT} has completed succesfully!\nBinaries available at https://build.nav.community/binaries/${BRANCH}"}' \
-    "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${PR_NUMBER}/comments"
+    "https://api.github.com/repos/${REPO}/navcoin-core/issues/${PR}/comments"
 fi
 
 echo $LASTCOMMIT >  ~/public_html/binaries/$BRANCH/.lastbuild
