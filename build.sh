f71ae6f9a21e2cf9df242c9638b47898e4385f83
navbuilder <57806386+navbuilder@users.noreply.github.com>
2019-11-15 19:01:42 +0100

    Touch sha256sum

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ mkdir -p ~/public_html/binaries/$BRANCH/log/osx
 mkdir -p ~/public_html/binaries/$BRANCH/log/linux
 
 touch ~/public_html/binaries/$BRANCH/.lastbuild
+touch ~/public_html/binaries/$BRANCH/$BRANCH.SHA256SUM.asc
 
 LASTCOMMIT=$(git log -n 1 --pretty=format:"%H")
 LASTBUILD=$(cat ~/public_html/binaries/$BRANCH/.lastbuild)
@@ -23,30 +24,29 @@ then
    exit 1
 fi
 
-make -C ~/navcoin-core/depends download SOURCES_PATH=`pwd`/cache/common
 cd ~/gitian-builder &&\
 USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY} -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-win.yml &&\
 mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz ~/public_html/binaries/$BRANCH/ &&\
 cp var/install.log ~/public_html/binaries/$BRANCH/log/win/ &&\
 cp var/build.log ~/public_html/binaries/$BRANCH/log/win/ &&\
-cd ~/public_html/binaries/$BRANCH && rm *SHA256SUM* ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
+cd ~/public_html/binaries/$BRANCH ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
 
 cd ~/gitian-builder &&\
 USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY}  -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-osx.yml &&\
 mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz ~/public_html/binaries/$BRANCH/ &&\
 cp var/install.log ~/public_html/binaries/$BRANCH/log/osx/ &&\
 cp var/build.log ~/public_html/binaries/$BRANCH/log/osx/ &&\
-cd ~/public_html/binaries/$BRANCH && rm *SHA256SUM* ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
+cd ~/public_html/binaries/$BRANCH ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
 
 cd ~/gitian-builder &&\
 USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY}  -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-linux.yml &&\
 mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz ~/public_html/binaries/$BRANCH/ &&\
 cp var/install.log ~/public_html/binaries/$BRANCH/log/linux/ &&\
 cp var/build.log ~/public_html/binaries/$BRANCH/log/linux/ &&\
-cd ~/public_html/binaries/$BRANCH && rm *SHA256SUM* ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
+cd ~/public_html/binaries/$BRANCH ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
 
 rm ~/public_html/binaries/$BRANCH/*debug* &&\
-cd ~/public_html/binaries/$BRANCH && rm *SHA256SUM* ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
+cd ~/public_html/binaries/$BRANCH ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
 
 echo $LASTCOMMIT >  ~/public_html/binaries/$BRANCH/.lastbuild
 
