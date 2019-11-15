b1e399b7270af350fbb934e3bf256a883c822e6b
navbuilder <57806386+navbuilder@users.noreply.github.com>
2019-11-15 22:16:09 +0100

    Fix GitHub comment

--- autobuild.sh
+++ autobuild.sh
@@ -13,12 +13,13 @@ do
     for pr in $DATA; do
         arrPR=(${pr//:/ })
         bash ~/build.sh ${arrPR[0]} ${arrPR[1]} ${arrIDS[$I]} && \
+        LASTCOMMIT=$(cat ~/public_html/binaries/${arrPR[1]}/.lastbuild) && \
         curl -s -H "Authorization: token ${ACCESS_TOKEN}" \
-         -X POST -d '{"body": "A new build of $(cat ~/public_html/binaries/${arrPR[1]}/.lastbuild) has completed succesfully!\nBinaries available at https://build.nav.community/binaries/${arrPR[0]}"}' \
+         -X POST -d '{"body": "A new build of ${LASTCOMMIT} has completed succesfully!\nBinaries available at https://build.nav.community/binaries/${arrPR[1]}"}' \
          "https://api.github.com/repos/${arrPR[0]}/navcoin-core/issues/${arrIDS[$I]}/comments"
         I=$((I+1))
     done
-    
+
     bash ~/build.sh navcoin master 0
 
     sleep 120
