e3fafefeabcbe0f890da7895d2eff0aa9aa0ad1b
navbuilder <57806386+navbuilder@users.noreply.github.com>
2019-11-16 13:18:27 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,22 +24,31 @@ then
    exit 1
 fi
 
-cd ~/gitian-builder &&\
-USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY} -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-win.yml &&\
+{
+  cd ~/gitian-builder &&\
+  USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY} -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-win.yml;
+} ||  cp var/*.log ~/public_html/binaries/$BRANCH/log/win/ && exit 1;
+
 mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz ~/public_html/binaries/$BRANCH/ &&\
 cp var/install.log ~/public_html/binaries/$BRANCH/log/win/ &&\
 cp var/build.log ~/public_html/binaries/$BRANCH/log/win/ &&\
 cd ~/public_html/binaries/$BRANCH ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
 
-cd ~/gitian-builder &&\
-USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY}  -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-osx.yml &&\
+{
+  cd ~/gitian-builder &&\
+  USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY}  -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-osx.yml;
+} || cp var/*.log ~/public_html/binaries/$BRANCH/log/osx/ && exit 1;
+
 mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz build/out/*.dmg ~/public_html/binaries/$BRANCH/ &&\
 cp var/install.log ~/public_html/binaries/$BRANCH/log/osx/ &&\
 cp var/build.log ~/public_html/binaries/$BRANCH/log/osx/ &&\
 cd ~/public_html/binaries/$BRANCH ; sha256sum n* > $BRANCH.SHA256SUM.asc &&\
 
-cd ~/gitian-builder &&\
-USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY}  -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-linux.yml &&\
+{
+  cd ~/gitian-builder &&\
+  USE_DOCKER=1 ./bin/gbuild --memory ${MEMORY}  -j${JOBS} --commit navcoin-core=${BRANCH} --url navcoin-core=${URL} ../navcoin-core/contrib/gitian-descriptors/gitian-linux.yml;
+} || cp var/*.log ~/public_html/binaries/$BRANCH/log/linux/ && exit 1;
+
 mv build/out/navcoin-*.tar.gz build/out/src/navcoin-*.tar.gz ~/public_html/binaries/$BRANCH/ &&\
 cp var/install.log ~/public_html/binaries/$BRANCH/log/linux/ &&\
 cp var/build.log ~/public_html/binaries/$BRANCH/log/linux/ &&\
@@ -53,3 +62,4 @@ echo $LASTCOMMIT >  ~/public_html/binaries/$BRANCH/.lastbuild
 sleep 30
 
 exit 0
+
