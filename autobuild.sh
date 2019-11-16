d932cff51e9cf0aa12101cad8d3247ffee22d1b6
navbuilder <57806386+navbuilder@users.noreply.github.com>
2019-11-16 18:12:37 +0100

    Fix quotes

--- autobuild.sh
+++ autobuild.sh
@@ -15,10 +15,11 @@ do
         bash ~/build.sh ${arrPR[0]} ${arrPR[1]} ${arrIDS[$I]} && \
         LASTCOMMIT=$(cat ~/public_html/binaries/${arrPR[1]}/.lastbuild) && \
         curl -s -H "Authorization: token ${ACCESS_TOKEN}" \
-         -X POST -d '{"body": "A new build of ${LASTCOMMIT} has completed succesfully!\nBinaries available at https://build.nav.community/binaries/${arrPR[1]}"}' \
+         -X POST -d "{\"body\": \"A new build of ${LASTCOMMIT} has completed succesfully!\nBinaries available at https://build.nav.community/binaries/${arrPR[1]}\"}" \
          "https://api.github.com/repos/navcoin/navcoin-core/issues/${arrIDS[$I]}/comments"
         I=$((I+1))
     done
+
 
     bash ~/build.sh navcoin master 0
 
